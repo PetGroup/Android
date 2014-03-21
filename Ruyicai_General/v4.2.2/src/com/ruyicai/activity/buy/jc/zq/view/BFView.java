30d8515fd6527cbdf0dbf0560ff33cef0b70f4e6
yejc <yejianchun@ruyicai.com>
2014-03-21 14:14:52 +0800

    v4.2.2code

---
+++ Ruyicai_General/v4.2.2/src/com/ruyicai/activity/buy/jc/zq/view/BFView.java
@@ -0,0 +1,314 @@
+package com.ruyicai.activity.buy.jc.zq.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.palmdream.RuyicaiAndroid.R;
+import com.ruyicai.activity.buy.jc.JcCommonMethod;
+import com.ruyicai.activity.buy.jc.JcMainView;
+import com.ruyicai.activity.buy.jc.oddsprize.JCPrizePermutationandCombination;
+import com.ruyicai.activity.common.CommonViewHolder;
+import com.ruyicai.code.jc.zq.FootBF;
+import com.ruyicai.constant.Constants;
+import com.ruyicai.net.newtransaction.pojo.BetAndGiftPojo;
+import com.ruyicai.util.PublicMethod;
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * 大小分
+ * 
+ * @author Administrator
+ * 
+ */
+public class BFView extends JcMainView {
+	private final int MAX_TEAM = 4;
+	FootBF footbfCode;
+
+	public BFView(Context context, BetAndGiftPojo betAndGift, Handler handler,
+			LinearLayout layout, String type, boolean isdanguan,
+			List<String> checkTeam) {
+		super(context, betAndGift, handler, layout, type, isdanguan, checkTeam);
+		footbfCode = new FootBF(context);
+	}
+
+	public void setDifferValue(JSONObject jsonItem, Info itemInfo)
+			throws JSONException {
+		itemInfo.vStrs = new String[31];
+		for (int j = 0; j < itemInfo.vStrs.length; j++) {
+			itemInfo.getVStrs()[j] = jsonItem.getString("score_v"
+					+ FootBF.bqcType[j]);
+		}
+	}
+
+	/**
+	 * 获取单关投注的中奖金额最大金额和最小金额的字符串
+	 * 
+	 * @return
+	 */
+	public String getDanPrizeString(int muti) {
+
+		return JCPrizePermutationandCombination.getNewDanGuanPrize(
+				getOddsArraysValue(), muti);
+	}
+
+	@Override
+	public int getTeamNum() {
+		return MAX_TEAM;
+	}
+
+	@Override
+	public BaseExpandableListAdapter getAdapter() {
+		return adapter;
+	}
+
+	@Override
+	public String getLotno() {
+		return Constants.LOTNO_JCZQ_BF;
+	}
+
+	@Override
+	public String getTitle() {
+		if (isDanguan) {
+			return context.getString(R.string.jczq_sfc_danguan_title)
+					.toString();
+		} else {
+			return context.getString(R.string.jczq_sfc_guoguan_title)
+					.toString();
+		}
+	}
+
+	@Override
+	public String getTypeTitle() {
+		return context.getString(R.string.jczq_dialog_sfc_guoguan_title)
+				.toString();
+	}
+
+	/**
+	 * 
+	 * 获取注码
+	 * 
+	 */
+	public String getCode(String key, List<Info> listInfo) {
+		return footbfCode.getCode(key, listInfo);
+	}
+
+	/**
+	 * 
+	 * 获取注码
+	 * 
+	 */
+	public String getAlertCode(List<Info> listInfo) {
+		String codeStr = "";
+		for (int i = 0; i < listInfo.size(); i++) {
+			Info info = (Info) listInfo.get(i);
+			if (info.onclikNum > 0) {
+				codeStr += PublicMethod.stringToHtml(info.getWeeks() + " " + info.getTeamId(), 
+						Constants.JC_TOUZHU_TITLE_TEXT_COLOR);
+				codeStr += info.getHome() + " vs " + info.getAway() + "<br>比分：";
+				for (int j = 0; j < info.check.length; j++) {
+					if (info.check[j].isChecked()) {
+						codeStr += PublicMethod.stringToHtml(info.check[j].getChcekTitle(), 
+								Constants.JC_TOUZHU_TEXT_COLOR) + "  ";
+					}
+				}
+				if (info.isDan()) {
+					codeStr += PublicMethod.stringToHtml("(胆)", Constants.JC_TOUZHU_TEXT_COLOR)+ "  ";
+				}
+				codeStr += "<br>";
+			}
+		}
+		return codeStr;
+	}
+
+	/**
+	 * 初始化列表
+	 */
+	public void initListView(ExpandableListView listview, Context context,
+			List<List> listInfo) {
+		adapter = new JcInfoExpandableListAdapter(context, listInfo);
+		listview.setAdapter(adapter);
+	}
+	
+	public class JcInfoExpandableListAdapter extends BaseExpandableListAdapter {
+		private LayoutInflater mInflater; // 扩充主列表布局
+		private List<List> mList;
+		public JcInfoExpandableListAdapter(Context context, List<List> list) {
+			mInflater = LayoutInflater.from(context);
+			mList = list;
+		}
+
+		@Override
+		public int getGroupCount() {
+			if (mList == null) {
+				return 0;
+			}
+			return mList.size();
+		}
+
+		@Override
+		public int getChildrenCount(int groupPosition) {
+			ArrayList<Info> list = (ArrayList<Info>) mList.get(groupPosition);
+			if (list == null) {
+				return 0;
+			}
+			return list.size();
+		}
+
+		@Override
+		public Object getGroup(int groupPosition) {
+			return mList.get(groupPosition);
+		}
+
+		@Override
+		public Object getChild(int groupPosition, int childPosition) {
+			ArrayList<Info> list = (ArrayList<Info>) mList.get(groupPosition);
+			if (list == null) {
+				return null;
+			}
+			return list.get(childPosition);
+		}
+
+		@Override
+		public long getGroupId(int groupPosition) {
+			return groupPosition;
+		}
+
+		@Override
+		public long getChildId(int groupPosition, int childPosition) {
+			return childPosition;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
+			return getConvertView(groupPosition, isExpanded, convertView, mList, mInflater);
+		}
+
+		@Override
+		public View getChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
+			CommonViewHolder.ChildViewHolder holder = null;
+			final ArrayList<Info> list = (ArrayList<Info>) mList.get(groupPosition);
+			final Info info = list.get(childPosition);
+			if (convertView == null) {
+				holder = new CommonViewHolder.ChildViewHolder();
+				convertView = mInflater.inflate(
+						R.layout.buy_jc_main_listview_item_others, null);
+				holder = JcCommonMethod.initChildViewHolder(convertView);
+				convertView.setTag(holder);
+			} else {
+				holder = (CommonViewHolder.ChildViewHolder) convertView.getTag();
+			}
+			ViewOnClickListener listener = new ViewOnClickListener(holder, info);
+			holder.btnShowDetail.setOnClickListener(listener);
+			holder.analysis.setOnClickListener(listener);
+			JcCommonMethod.setDividerShowState(childPosition, holder);
+			JcCommonMethod.setTeamTime(info, holder);
+			JcCommonMethod.setJcZqTeamName(info, holder);
+			JcCommonMethod.setBtnText(info, holder);
+			setDanShowState(info, holder);
+
+			if (isDanguan || isHunHe()) {
+				holder.btnDan.setVisibility(Button.GONE);
+			} else {
+				holder.btnDan.setVisibility(Button.VISIBLE);
+				holder.btnDan.setOnClickListener(listener);
+			}
+			
+			return convertView;
+		}
+
+		@Override
+		public boolean isChildSelectable(int groupPosition, int childPosition) {
+			return false;
+		}
+	}
+	
+	public class ViewOnClickListener implements View.OnClickListener {
+		private CommonViewHolder.ChildViewHolder holder;
+		private Info info;
+		public ViewOnClickListener(CommonViewHolder.ChildViewHolder holder, Info info) {
+			this.holder = holder;
+			this.info = info;
+		}
+
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.jc_main_list_item_button:
+				showDetail();
+				MobclickAgent.onEvent(context, "jczqbifen_duizhenxiangqing");
+				break;
+				
+			case R.id.game_dan:
+				setGameDanShowState(info, holder);
+				MobclickAgent.onEvent(context, "jczqbifen_dan");
+				break;
+				
+			case R.id.game_analysis:
+				trunExplain(getEvent(Constants.JCFOOT, info),
+						info.getHome(), info.getAway());
+				MobclickAgent.onEvent(context, "jczqbifen_fenxi");
+				break;
+			}
+		}
+		
+		private void showDetail() {
+			if (info.onclikNum > 0 || isCheckTeam()) {
+				info.setLotno(Constants.LOTNO_JCZQ_BF);
+				info.createDialog(FootBF.titleStrs, true,
+						info.getHome() + " VS " + info.getAway());
+			}
+			isNoDan(info, holder.btnDan);
+		}
+		
+	}
+
+	@Override
+	public String getPlayType() {
+		if (isDanguan) {
+			return "J00002_0";
+		} else {
+			return "J00002_1";
+		}
+	}
+
+	@Override
+	public List<double[]> getOdds(List<Info> listInfo) {
+		return footbfCode.getOddsList(listInfo);
+	}
+
+	/**
+	 * 最多可以设胆7场比赛
+	 */
+	public boolean isDanCheckTeam() {
+		int teamNum = initDanCheckedNum();
+		if (teamNum < MAX_TEAM - 1) {
+			return true;
+		} else {
+			Toast.makeText(context, "您最多可以选择" + (MAX_TEAM - 1) + "场比赛进行设胆！",
+					Toast.LENGTH_SHORT).show();
+			return false;
+		}
+	}
+
+}
---
+++ Ruyicai_General/v4.2.2/src/com/ruyicai/data/net/GetGYJTeamInfoAsyncTask.java
@@ -0,0 +1,221 @@
+package com.ruyicai.data.net;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.widget.Toast;
+import com.ruyicai.constant.Constants;
+import com.ruyicai.model.ChampionshipBean;
+import com.ruyicai.model.RequestResultsBean;
+import com.ruyicai.net.InternetUtils;
+import com.ruyicai.util.ProtocolManager;
+import com.ruyicai.util.PublicMethod;
+import com.ruyicai.util.json.JsonUtils;
+
+public class GetGYJTeamInfoAsyncTask {
+	public static final String WORLDCUP_ISSUE = "2014001";
+	public static final String EUROPE_ISSUE = "2014002";
+	private static GetGYJTeamInfoAsyncTask sInstance;
+//	private String[] issueArray = null;
+	private String championsType = "1";
+	private String worldCupType = "2";
+	private ProgressDialog progressdialog;
+	private String command = "jingCaiZq";
+	private String issue = "gyjIssue";
+	private String against= "gyjDuiZhen";
+	private Context context;
+	private Handler handler;
+//	private String[] endTime = null;
+	private Map<String, String> infoMap = null;
+	
+	public GetGYJTeamInfoAsyncTask(Context context, Handler handler) {
+		this.context = context;
+		this.handler = handler;
+	}
+
+	public static GetGYJTeamInfoAsyncTask getInstance(Context context, Handler handler) {
+		if (sInstance == null) {
+			sInstance = new GetGYJTeamInfoAsyncTask(context, handler);
+		} else {
+			sInstance.context = context;
+			sInstance.handler = handler;
+		}
+		return sInstance;
+	}
+	
+	/**
+	 * 获取欧冠杯数据
+	 */
+	public void getEuropeInfo() {
+		new GetIssueAsyncTask().execute(1);
+	}
+	
+	/**
+	 * 获取世界杯数据
+	 */
+	public void getworldCupInfo() {
+		if (infoMap == null) {
+			new GetIssueAsyncTask().execute(0);
+		} else {
+			if (infoMap.containsKey(WORLDCUP_ISSUE)) {
+				progressdialog = PublicMethod.creageProgressDialog(context);
+				new GetTeamAgainstAsyncTask().execute(WORLDCUP_ISSUE, worldCupType);
+			}
+		}
+//		if (issueArray == null) {
+//			new GetIssueAsyncTask().execute(0);
+//		} else {
+//			progressdialog = PublicMethod.creageProgressDialog(context);
+//			new GetTeamAgainstAsyncTask().execute(issueArray[0], worldCupType);
+//		}
+	}
+	
+//	public String[] getEndTime() {
+//		return endTime;
+//	}
+	
+//	public String getIssue(int index) {
+//		if (issueArray != null && issueArray.length >index) {
+//			if (index == 0) {
+//				return issueArray[1];
+//			} else {
+//				return issueArray[0];
+//			}
+//		}
+//		return "";
+//	}
+	
+	public Map<String, String> getInfoMap() {
+		return infoMap;
+	}
+
+
+	/**
+	 * 获取期号异步任务类
+	 */
+	private class GetIssueAsyncTask extends AsyncTask<Integer, String, String> {
+		private int index = 0;
+		@Override
+		protected void onPreExecute() {
+			progressdialog = PublicMethod.creageProgressDialog(context);
+		}
+		
+		@Override
+		protected String doInBackground(Integer... params) {
+			try {
+				index  = params[0];
+				JSONObject jsonProtocol = ProtocolManager.getInstance()
+						.getDefaultJsonProtocol();
+				jsonProtocol.put(ProtocolManager.COMMAND, command);
+				jsonProtocol.put(ProtocolManager.REQUESTTYPE, issue);
+				jsonProtocol.put(ProtocolManager.LOTNO, Constants.LOTNO_JCZQ_GJ);
+				return InternetUtils.GetMethodOpenHttpConnectSecurity(
+						Constants.LOT_SERVER, jsonProtocol.toString());
+			} catch (JSONException e) {
+				e.printStackTrace();
+				PublicMethod.closeProgressDialog(progressdialog);
+			}
+			return "";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (!TextUtils.isEmpty(result)) {
+				try {
+					RequestResultsBean resultBean = JsonUtils.resultData(result, RequestResultsBean.class);
+					if (resultBean != null) {
+						if ("0000".equals(resultBean.getError_code())) {
+							JSONArray jsonArray = new JSONArray(resultBean.getResult());
+//							issueArray = new String[jsonArray.length()];
+//							endTime = new String[jsonArray.length()];
+							infoMap = new HashMap<String, String>();
+							for (int i = 0; i < jsonArray.length(); i++) {
+								String jsonStr = jsonArray.getString(i);
+								String issue = JsonUtils.readjsonString("batchCode", jsonStr);
+								String endTiem = JsonUtils.readjsonString("endtime", jsonStr);
+								infoMap.put(issue, endTiem);
+//								issueArray[i] = JsonUtils.readjsonString("batchCode", jsonStr);
+//								endTime[i] = JsonUtils.readjsonString("endtime", jsonStr);
+							}
+							if (index == 0) {
+								if (infoMap.containsKey(WORLDCUP_ISSUE)) {
+									new GetTeamAgainstAsyncTask().execute(WORLDCUP_ISSUE, worldCupType);
+								}
+							} else {
+								if (infoMap.containsKey(EUROPE_ISSUE)) {
+									new GetTeamAgainstAsyncTask().execute(EUROPE_ISSUE, championsType);
+								}
+							}
+						} else {
+							Toast.makeText(context, resultBean.getMessage(), Toast.LENGTH_SHORT).show();;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			PublicMethod.closeProgressDialog(progressdialog);
+		}
+	}
+	
+	/**
+	 * 获取对阵异步任务类
+	 */
+	private class GetTeamAgainstAsyncTask extends AsyncTask<String, String, String> {
+		private String type = "";
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				type = params[1];
+				JSONObject jsonProtocol = ProtocolManager.getInstance()
+						.getDefaultJsonProtocol();
+				jsonProtocol.put(ProtocolManager.COMMAND, command);
+				jsonProtocol.put(ProtocolManager.REQUESTTYPE, against);
+				jsonProtocol.put(ProtocolManager.LOTNO, Constants.LOTNO_JCZQ_GJ);
+				jsonProtocol.put(ProtocolManager.BATCHCODE, params[0]);
+				return InternetUtils.GetMethodOpenHttpConnectSecurity(
+						Constants.LOT_SERVER, jsonProtocol.toString());
+			} catch (JSONException e) {
+				e.printStackTrace();
+				PublicMethod.closeProgressDialog(progressdialog);
+			}
+			return "";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (!TextUtils.isEmpty(result)) {
+				try {
+					RequestResultsBean resultBean = JsonUtils.resultData(result, RequestResultsBean.class);
+					if (resultBean != null) {
+						if ("0000".equals(resultBean.getError_code())) {
+							List<ChampionshipBean> list = JsonUtils.getList(resultBean.getResult(), ChampionshipBean.class);
+							Message msg = Message.obtain(handler);
+							msg.obj = list;
+							if (championsType.equals(type)) {
+								msg.what = 0;
+							} else {
+								msg.what = 1;
+							}
+							handler.sendMessage(msg);
+						} else {
+							Toast.makeText(context, resultBean.getMessage(), Toast.LENGTH_SHORT).show();;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			PublicMethod.closeProgressDialog(progressdialog);
+		}
+	}
+}
