ecccefba11d72cfca775aed2d8a800cf2428f258
miqingqiang <miqingqiang@ruyicai.com>
2014-03-28 18:34:23 +0800

    update

--- Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/zixuan/AddView.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/zixuan/AddView.java
@@ -651,6 +651,15 @@ public class AddView {
 		private void addSeparator(String lotoNo, String touzhuType,
 				SpannableStringBuilder builder, int i) {
 
+			if(lotoNo.equals(Constants.LOTNO_JLK3)&& 
+					(touzhuType.equals("twosame_dan"))
+					&&!builder.toString().contains("#")){
+				deleteStringFromBuilder(builder,2,3,2,4);
+			}else if(lotoNo.equals(Constants.LOTNO_JLK3)&& 
+					(touzhuType.equals("twosame_fu"))
+					&&!builder.toString().contains("#")){
+				deleteStringFromBuilder(builder,3,4,3,6);
+			}
 			if (i != getCodes().size() - 1) {
 				if (lotoNo == null || touzhuType == null) {
 					builder.append("|");
@@ -679,11 +688,34 @@ public class AddView {
 						|| (lotoNo.equals(Constants.LOTNO_NMK3) && touzhuType
 								.equals("twosame_dan"))
 						|| (lotoNo.equals(Constants.LOTNO_CQ_ELVEN_FIVE) && touzhuType
-								.equals("dantuo"))) {
+								.equals("dantuo"))
+						||(lotoNo.equals(Constants.LOTNO_JLK3) && touzhuType
+										.equals("twosame_dan"))
+						||(lotoNo.equals(Constants.LOTNO_JLK3) && touzhuType
+												.equals("twosame_fu"))) {
 					builder.append("#");
 				} else {
 					builder.append("|");
 				}
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * 删除builder中指定字符
+	 * @param builder
+	 * @param numOne
+	 * @param numOther
+	 * @param numDis
+	 */
+	private void deleteStringFromBuilder(SpannableStringBuilder builder,int numOne,int numOther,int numDis,int multiple ){
+		int builderLength=(builder.length()+1)/multiple;
+		for(int temp=0;temp<builderLength;temp++){
+			if(temp>=1){
+				builder.delete(numOther*temp+1, numOther*temp+numDis);
+			}else{
+				builder.delete(1, numOne);
 			}
 		}
 	}
