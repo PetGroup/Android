4be20b734030d2a804535fab54792ba7da209033
miqingqiang <miqingqiang@ruyicai.com>
2014-04-18 11:23:35 +0800

    update

--- Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/guess/RuyiGuessDetailActivity.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/guess/RuyiGuessDetailActivity.java
@@ -1352,52 +1352,52 @@ public class RuyiGuessDetailActivity extends Activity implements IWXAPIEventHand
 //			initAccessToken(token, expires_in);
 //		}
 		
-		mWeiboAuth = new WeiboAuth(this, Constants.CONSUMER_KEY, Constants.CONSUMER_URL, Constants.SCOPE);
-		mWeiboAuth.anthorize(new AuthListener());
+//		mWeiboAuth = new WeiboAuth(this, Constants.CONSUMER_KEY, Constants.CONSUMER_URL, Constants.SCOPE);
+//		mWeiboAuth.anthorize(new AuthListener());
 		
 	}
 	
-	private WeiboAuth mWeiboAuth;
-	private Oauth2AccessToken mAccessToken;
-	
-	 class AuthListener implements WeiboAuthListener {
-	        
-	        @Override
-	        public void onComplete(Bundle values) {
-	            // 从 Bundle 中解析 Token
-	            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
-	            if (mAccessToken.isSessionValid()) {
-	                
-	                // 保存 Token 到 SharedPreferences
-	            	RW.writeAccessToken(RuyiGuessDetailActivity.this, mAccessToken);
-	                Toast.makeText(RuyiGuessDetailActivity.this, 
-	                        "授权成功", Toast.LENGTH_SHORT).show();
-	            } else {
-	                // 以下几种情况，您会收到 Code：
-	                // 1. 当您未在平台上注册的应用程序的包名与签名时；
-	                // 2. 当您注册的应用程序包名与签名不正确时；
-	                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
-	                String code = values.getString("code");
-	                String message = "授权失败";
-	                if (!TextUtils.isEmpty(code)) {
-	                    message = message + "\nObtained the code: " + code;
-	                }
-	                Toast.makeText(RuyiGuessDetailActivity.this, message, Toast.LENGTH_LONG).show();
-	            }
-	        }
+//	private WeiboAuth mWeiboAuth;
+//	private Oauth2AccessToken mAccessToken;
+//	
+//	 class AuthListener implements WeiboAuthListener {
+//	        
+//	        @Override
+//	        public void onComplete(Bundle values) {
+//	            // 从 Bundle 中解析 Token
+//	            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+//	            if (mAccessToken.isSessionValid()) {
+//	                
+//	                // 保存 Token 到 SharedPreferences
+//	            	RW.writeAccessToken(RuyiGuessDetailActivity.this, mAccessToken);
+//	                Toast.makeText(RuyiGuessDetailActivity.this, 
+//	                        "授权成功", Toast.LENGTH_SHORT).show();
+//	            } else {
+//	                // 以下几种情况，您会收到 Code：
+//	                // 1. 当您未在平台上注册的应用程序的包名与签名时；
+//	                // 2. 当您注册的应用程序包名与签名不正确时；
+//	                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+//	                String code = values.getString("code");
+//	                String message = "授权失败";
+//	                if (!TextUtils.isEmpty(code)) {
+//	                    message = message + "\nObtained the code: " + code;
+//	                }
+//	                Toast.makeText(RuyiGuessDetailActivity.this, message, Toast.LENGTH_LONG).show();
+//	            }
+//	        }
 
-	        @Override
-	        public void onCancel() {
-	            Toast.makeText(RuyiGuessDetailActivity.this, 
-	                    "取消授权", Toast.LENGTH_LONG).show();
-	        }
-
-	        @Override
-	        public void onWeiboException(WeiboException e) {
-	            Toast.makeText(RuyiGuessDetailActivity.this, 
-	                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
-	        }
-	    }
+//	        @Override
+//	        public void onCancel() {
+//	            Toast.makeText(RuyiGuessDetailActivity.this, 
+//	                    "取消授权", Toast.LENGTH_LONG).show();
+//	        }
+//
+//	        @Override
+//	        public void onWeiboException(WeiboException e) {
+//	            Toast.makeText(RuyiGuessDetailActivity.this, 
+//	                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
+//	        }
+//	    }
 	
 	private void initAccessToken(String token, String expires_in) {
 		Token accessToken = new Token(token, Weibo.getAppSecret());
