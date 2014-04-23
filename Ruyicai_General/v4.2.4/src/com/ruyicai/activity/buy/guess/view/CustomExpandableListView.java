ff0ff7bbe1a99fc3953b976a4402d5bb3b9460be
yejc <yejianchun@ruyicai.com>
2014-04-23 09:08:33 +0800

    modified

--- Ruyicai_General/v4.2.4/src/com/ruyicai/activity/buy/guess/view/CustomExpandableListView.java
+++ Ruyicai_General/v4.2.4/src/com/ruyicai/activity/buy/guess/view/CustomExpandableListView.java
@@ -18,10 +18,11 @@ import android.widget.ViewFlipper;
 
 public class CustomExpandableListView extends ExpandableListView {
 	private float mLastY = -1;
-	private int mTopMarginPx = 0;
-	private FrameLayout.LayoutParams mParams = null;
-	private int mDensity = 0;
-	private ViewFlipper mViewFlipper = null;
+	private int mTopMarginPx = 0;  //轮播广告控件距上边控件的距离
+	private FrameLayout.LayoutParams mParams = null; //轮播广告控件布局参数
+	private int mDensity = 0; //屏幕密度
+	private ViewFlipper mViewFlipper = null; //轮播广告控件
+//	private FrameLayout mAdvertisingLayout = null;
 
 	public CustomExpandableListView(Context context) {
 		super(context);
@@ -51,22 +52,34 @@ public class CustomExpandableListView extends ExpandableListView {
 			final float deltaY = ev.getRawY() - mLastY;
 			mLastY = ev.getRawY();
 			/**广告位向上滚动***/
-			if ((mParams.topMargin >= -mTopMarginPx && mParams.topMargin <= 0)
-					|| (getFirstVisiblePosition() == 0 && deltaY > 0)) {
-				int topMargin = (int) (mDensity * deltaY) + mParams.topMargin;
-				if (topMargin < -mTopMarginPx) {
-					topMargin = -mTopMarginPx;
-				} else if (topMargin > 0) {
-					topMargin = 0;
+			//在滑动时会调用多次 尽量不要在这里创建对象
+			if (deltaY > 0) {
+				if (getFirstVisiblePosition() == 0) {
+					PublicMethod.setMargin(mTopMarginPx, deltaY, mDensity,
+							mParams, mViewFlipper);
 				}
-				mParams.setMargins(0, topMargin, 0, 0);
-				mViewFlipper.setLayoutParams(mParams);
+			} else {
+				PublicMethod.setMargin(mTopMarginPx, deltaY, mDensity, mParams,
+						mViewFlipper);
 			}
+			
+//			if ((mParams.topMargin >= -mTopMarginPx && mParams.topMargin <= 0)
+//					|| (getFirstVisiblePosition() == 0 && deltaY > 0)) {
+//				int topMargin = (int) (mDensity * deltaY) + mParams.topMargin;
+//				if (topMargin < -mTopMarginPx) {
+//					topMargin = -mTopMarginPx;
+//				} else if (topMargin > 0) {
+//					topMargin = 0;
+//				}
+//				mParams.setMargins(0, topMargin, 0, 0);
+//				mViewFlipper.setLayoutParams(mParams);
+//			}
 			/**广告位向上滚动***/
 			break;
 		}
 		return super.onTouchEvent(ev);
 	}
+	
 	
 	private void init(Context context) {
 		mTopMarginPx = PublicMethod.getPxInt(150, getContext());
@@ -75,9 +88,10 @@ public class CustomExpandableListView extends ExpandableListView {
 				mTopMarginPx);
 		mDensity = (int)PublicMethod.getDensity(context);
 	}
-	
+
 	public void setmViewFlipper(ViewFlipper mViewFlipper) {
 		this.mViewFlipper = mViewFlipper;
 	}
+
 
 }
