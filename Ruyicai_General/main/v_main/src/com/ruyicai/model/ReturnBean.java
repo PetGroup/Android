1c3f51c3569161ae923da552e1450c8bccf8b959
server <yejianchun@ruyicai.com>
2014-04-29 11:22:19 +0800

    修改数据模型序列化方式

--- Ruyicai_General/main/v_main/src/com/ruyicai/model/ReturnBean.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/model/ReturnBean.java
@@ -1,8 +1,10 @@
 package com.ruyicai.model;
 
-import java.io.Serializable;
 
-public class ReturnBean implements Serializable{
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ReturnBean implements Parcelable{
 	private static final long serialVersionUID = -8704930601795903613L;
 	private String error_code = "";
 	private String message = "";
@@ -25,4 +27,33 @@ public class ReturnBean implements Serializable{
 	public void setResult(String result) {
 		this.result = result;
 	}
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(error_code);
+		dest.writeString(message);
+		dest.writeString(result);
+	}
+	
+	public static final Parcelable.Creator<ReturnBean> CREATOR = new Parcelable.Creator<ReturnBean>() {
+
+		@Override
+		public ReturnBean createFromParcel(Parcel source) {
+			ReturnBean bean = new ReturnBean();
+			bean.error_code = source.readString();
+			bean.message = source.readString();
+			bean.result = source.readString();
+			return bean;
+		}
+
+		@Override
+		public ReturnBean[] newArray(int size) {
+			return new ReturnBean[size];
+		}
+		
+	};
+	
 }
